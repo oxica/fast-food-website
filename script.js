db7ca5d22bdd0347ead611258b330ccfaea70d01
oxica <kskaplun@gmail.com>
2022-03-30 01:08:09 +0300

    add navbar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 
@@ -15,5 +22,3 @@ setInterval(() => {
     icon.nextElementSibling.classList.add("change");
   }
 }, 4000);
-
-
